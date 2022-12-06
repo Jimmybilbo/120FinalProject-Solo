ae361961030a890f9401e92514ad92217700f013
SamDuncan <jacobsteck@outlook.com>
2022-12-06 10:02:36 -0500

    Trying to add 25 elements through JS when the page loads

--- resources/menu-admin_brain.js
+++ resources/menu-admin_brain.js
@@ -10,4 +10,24 @@
 //     <button class="BTN-ADD" onclick="addItem()" type="button">ADD TO CART</button> `;
 // menuItemTemplate.classList.add('menu-item');
 
-const menuItemListing = document.querySelector("#listing")
+
+function itemListing(name, price, img, desc) {
+    const menuItemListing = document.querySelector("#listing")
+
+    menuItemListing.innerHTML += `
+        <div class="menu-item">
+
+            <img src="${img}" alt="" class="menu-pic">
+            <button class="menu-btn">
+                <h3 class="menu-name">${name}</h3>
+            </button>
+            <span class="menu-price">$${price}</span>
+            <button class="menu-btn">
+                <h3 class="menu-name">${desc}</h3>
+            </button>
+            <button class="BTN-ADD" onclick="addItem(['burger', 52.99, 'resources/images/burger-img.png', 1])" type="button">ADD TO CART</button>
+            
+        </div> `;
+    // menuItemListing.classList.add('menu-item');
+}
+
