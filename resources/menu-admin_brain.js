94abcb99645b804e9fd34ae134e00bd69318ac47
SamDuncan <jacobsteck@outlook.com>
2022-12-08 10:59:01 -0500

    Cleaning up the garbage... I made adding an item work but then it broke... for some reason you can only add one item... I am thinking I need a loop but I dont want it to keep going.

--- resources/menu-admin_brain.js
+++ resources/menu-admin_brain.js
@@ -1,17 +1,28 @@
-function itemListing(name, price, img, desc) {
-    const menuItemListing = document.querySelector("#listing")
+var menuItemListing = document.querySelector("#listing")
+
+function itemListing(img) {
+    let name = prompt("Product Name");
+    // validation has to be only letters
+
+    let dollar = prompt("Product Price");
+    // validation has to be only numbers greater than 0 but no more than 4 characters in length
+    let price = `${dollar}.00`
+
+    let desc = prompt("Product Description")
+    // any and all... if nothing is input, the description reads -No Description-
 
     menuItemListing.innerHTML += `
         <div class="menu-item">
-            <img src="${img}" alt="" class="menu-pic">
-            <h3 class="menu-name">${name}</h3>
-            <span class="menu-price">$${price}</span>
+            <img src="${img}" alt="item">
+            <h3>${name}</h3>
+            <span>$${price}</span>
             <p>${desc}</p>
         </div> `;
 }
 
+
 // nested array that holds all the listings... each category is hidden and must be revealed by sending a number 1-5 for what should be displayed
 
-document.querySelector("#listing").addEventListener("click", e => {
-    itemListing('beenis', 50002.99, 'resources/Images/ItemImages/NoPhoto_Default.jpg', 'This is a test description.');
+menuItemListing.querySelector("#whoops").addEventListener("click", e => {
+    itemListing('resources/Images/ItemImages/NoPhoto_Default.jpg');
 }); //---Creates listing on click endlessly---
