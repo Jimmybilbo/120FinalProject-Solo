afb77e175b10eb13f19819f6cc13d7062181af90
SamDuncan <jacobsteck@outlook.com>
2022-12-11 15:04:43 -0500

    the html and css is done being fucked with... now I just need to add the admin edits toggle... I forgot about the remove item bit so that too, and then I need to do the input validation. I will then move into making a basic header/footer and then the cart functionality...

--- resources/menu-admin_brain.js
+++ resources/menu-admin_brain.js
@@ -1,7 +1,9 @@
-var menuItemListing = document.querySelector("#listing--section")
-
 function itemListing() {
-    let img = "resources/Images/ItemImages/NoPhoto_Default.jpg"
+    let location = prompt("Where would you like this listing placed?\n(Entrees[0] / Drinks[1] / Sides[2] / Ingredients[3] / Accessories[4])");
+    // validation has can be word or number, word has to be exact match (non case sensitive), number must be 0-4
+    let menuItemListing = document.querySelector(`#${location}--section`);
+
+    let img = "NoPhoto_Default.jpg"
     let name = prompt("Product Name");
     // validation has to be only letters
 
@@ -9,14 +11,15 @@ function itemListing() {
     // validation has to be only numbers greater than 0 but no more than 4 characters in length
     let price = `${dollar}.00`
 
-    let desc = prompt("Product Description")
+    let desc = prompt("Product Description");
     // any and all... if nothing is input, the description reads -No Description-
+
 
     menuItemListing.innerHTML += `
         <div class="item">
-            <img src="${img}" alt="item">
+            <img src="resources/Images/ItemImages/${img}" alt="item">
             <div class="item--alignment">
-                <h3>${name}</h3>
+                <p>${name}</p>
                 <p>$${price}</p>
             </div>
             <p class="item--description">${desc}</p>
