3be0ee5c0af4f71715470e6a131660c03e4a0dcf
SamDuncan <jacobsteck@outlook.com>
2022-12-12 08:25:59 -0500

    script works, now, validation

--- resources/menu-admin_brain.js
+++ resources/menu-admin_brain.js
@@ -1,4 +1,8 @@
+var itemIndex = 24
+
 function itemListing() {
+    itemIndex += 1;
+
     let location = prompt("Where would you like this listing placed?\n(Entrees[0] / Drinks[1] / Sides[2] / Ingredients[3] / Accessories[4])");
     // validation has can be word or number, word has to be exact match (non case sensitive), number must be 0-4
     let menuItemListing = document.querySelector(`#${location}--section`);
@@ -15,14 +19,17 @@ function itemListing() {
     // any and all... if nothing is input, the description reads -No Description-
 
     menuItemListing.innerHTML += `
-        <div class="item">
+        <div class="item" id="item${itemIndex}">
             <img src="resources/Images/ItemImages/${img}" alt="item">
             <div class="item--alignment">
                 <p>${name}</p>
                 <p>$${price}</p>
             </div>
             <p class="item--description">${desc}</p>
-            <button class="item--toCart">Add to Cart</button>
+            <div class="button--selection">
+                <button class="item--button" id="toCart" onclick="">Add to Cart</button>
+                <button class="item--button" onclick="itemRemoval('item${itemIndex}')">Remove Item</button>
+            </div>
         </div> `;
 }
 
