7f51c5838bf4f025644d2afe8b3789c8f55bee02
SamDuncan <jacobsteck@outlook.com>
2022-12-13 10:47:42 -0500

    footer done. I have to switch gears for my other final for a second... during that time, I will decide what to do with cart display

--- resources/header-footer_brain.js
+++ resources/header-footer_brain.js
@@ -6,10 +6,10 @@ function addSticky() {
 
     if (window.pageYOffset > sticky) {
         nav.classList.add("nav--sticky");
-        nav.classList.remove("nav--transparent");
+        // nav.classList.remove("nav--transparent");
     }
     else {
         nav.classList.remove("nav--sticky");
-        nav.classList.add("nav--transparent");
+        // nav.classList.add("nav--transparent");
     }
 }
